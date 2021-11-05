27156c3724a1f1e8f172a56e3827cb9d5f5a3b83
Jonathan Oreste Maruca <marj3220@usherbrooke.ca>
2021-11-05 17:26:27 -0400

    Twin Temp Sensor on each mux output

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -4,42 +4,74 @@
 #include "Adafruit_SGP30.h"
 #include "SparkFun_I2C_Mux_Arduino_Library.h"
 
-Adafruit_HTU31D htu = Adafruit_HTU31D();
+#define NUMBER_OF_TEMP_SENS 4
 //Adafruit_SGP30 sgp = Adafruit_SGP30();
 QWIICMUX myMux;
-
 uint32_t timestamp;
+
+Adafruit_HTU31D **temperature_sensors;
 
 void purgeMoisture(int time_delay)
 {
-  htu.enableHeater(true);
+  for (byte i = 0; i < NUMBER_OF_TEMP_SENS; i = i + 2)
+  {
+    myMux.setPort(i);
+    for (byte j = 0; j < 2; j++)
+    {
+      temperature_sensors[i + j]->enableHeater(true);
+    }
+  }
   delay(time_delay);
-  htu.enableHeater(false);
 }
 
 void setup()
 {
   Serial.begin(115200);
   Wire.begin();
-  while (!Serial)
+  temperature_sensors = new Adafruit_HTU31D *[NUMBER_OF_TEMP_SENS];
+  for (int x = 0; x < NUMBER_OF_TEMP_SENS; x++)
   {
-    delay(10); //wait till serial port opens
+    temperature_sensors[x] = new Adafruit_HTU31D();
   }
-  if (!htu.begin(0x40))
+  if (myMux.begin() == false)
   {
-    Serial.println("Couldn't find the sensor!");
+    Serial.println("Mux not detected. Freezing...");
+    while (1)
+      ;
+  }
+  byte i = 0;
+  for (byte x = 0; x < NUMBER_OF_TEMP_SENS; x = x + 2)
+  {
+    myMux.setPort(i);
+    if (!temperature_sensors[x]->begin(0x40) || !temperature_sensors[x + 1]->begin(0x41))
+    {
+      Serial.println("Couldn't find sensor!");
+      while (1)
+        ;
+    }
+    i++;
   }
   purgeMoisture(5000);
-  timestamp = millis();
 }
 
 void loop()
 {
-  sensors_event_t humidity, temp;
-  htu.getEvent(&humidity, &temp);
-  //sgp.eCO2;
-  //sgp.TVOC;
-  Serial.println(humidity.relative_humidity);
-  Serial.println(temp.temperature);
-  timestamp = millis();
+  for (byte i = 0; i < NUMBER_OF_TEMP_SENS / 2; i++)
+  {
+    myMux.setPort(i);
+    for (byte j = 0; j < 2; j++)
+    {
+      sensors_event_t humidity, temp;
+      temperature_sensors[i + j]->getEvent(&humidity, &temp);
+      Serial.print("Sensor ");
+      Serial.print(i);
+      Serial.print(" Id ");
+      Serial.print(j);
+      Serial.println();
+      Serial.println(humidity.relative_humidity);
+      Serial.println(temp.temperature);
+      Serial.println();
+    }
+  }
+  delay(2000);
 }
