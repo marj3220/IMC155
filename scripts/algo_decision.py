19f60586a89948f4c773a02665bc9fb2cedb3450
Miklab65 <93946073+Miklab65@users.noreply.github.com>
2021-11-11 18:35:04 -0500

    Add files via upload

---
+++ scripts/algo_decision.py
@@ -0,0 +1,27 @@
+#Matrice 3x3 test chaque ligne (vecteur) est une bande de 3 capteurs
+T=[
+    [1,2,4],
+    [3,4,2],
+    [2,3,3],
+    [4,4,3]
+    ]
+nb_strip = len(T) # nombre de strip de capteur
+nb_sensor = len(T[0]) # nombre de capteur par strip
+d_capt = 1 # Distance entre les capteurs sur une même bande
+d_strip = 1 # Distance entre les bandes
+T_seuil = 3 # Température de seuil (si plus bas = alerte)
+# algo de décision
+flag_T = [[0 for i in range(nb_sensor)] for j in range(nb_strip)]
+for i in range(len(T)):
+    for j in range(len(T[1])):
+        if T[i][j] <= T_seuil:
+            flag_T[i][j] = 'We good'
+        else:
+            flag_T[i][j] = "Get u're ass outside!"
+#display
+print('\n')
+print('T = ',T)
+print('Nombre de bandes : ',nb_strip)
+print('Nombre de capteurs par bandes :',nb_sensor)
+print('État a chaque capteurs :\n',flag_T)
+print('\n')    
