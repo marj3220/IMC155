a27d509356eee17f82e5f3b4e93858f5c7c1a5a1
Miklab65 <93946073+Miklab65@users.noreply.github.com>
2021-11-19 13:28:24 -0500

    Update algo_decision.py

--- scripts/algo_decision.py
+++ scripts/algo_decision.py
@@ -9,34 +9,35 @@ last_T=[
     [5,5,5],
     ]
 T=[
-    [5,5,5],
-    [5,5,5]
+    [3,3,3],
+    [3,3,3]
     ]
 nb_strip = len(T) # nombre de strip de capteur
 nb_sensor = len(T[0]) # nombre de capteur par strip
 T_seuil = 3 # Température de seuil (si plus bas = alerte)
 # Flag sur les températures basses 
-heat = [[0 for i in range(3*nb_sensor)] for j in range(2*nb_strip+1)]
+heat = [[0 for i in range(2*nb_sensor+1)] for j in range(2*nb_strip+1)]
 for i in range(nb_strip):
     for j in range(nb_sensor):
         if T[i][j] <= T_seuil: # dans ce cas, valeur dans flag = 1 sinon reste 0
+            T[i][j] = 1
             #if last_T[i][j] > T_seuil: # notification par textos
                 #alerter.sendAlert("Indice de gel au capteur " + str(j+1) + " de la rangée " + str(i+1))
             if i == 0 and j == 0:
-                i_heat = i + 1
-                j_heat = i_heat
+                i_heat = i
+                j_heat = j + 1
             elif i == 0:
-                i_heat = i + 1
+                i_heat = i
                 if j == nb_sensor - 1:
-                    j_heat = 2 ** j - 1
+                    j_heat = 2 ** j +1
                 else:
                     j_heat = 2 ** j +1
-            elif j == 0: # pense pas j == 0 nécessaire....
+            elif j == 0:
                 j_heat = j + 1
                 if i == nb_strip - 1:
                     i_heat = 2 ** i -1
                 else:
-                    i_heat = 2 ** i + 1
+                    i_heat = 3 ** i
             elif i == nb_strip - 1 and j == nb_sensor - 1:
                 i_heat = 2 ** i - 1
                 j_heat = 2 ** j - 1
@@ -47,15 +48,16 @@ for i in range(nb_strip):
                 i_heat = 2 ** i - 1
                 j_heat = 2 ** j + 1
             else:
-                i_heat = 2 ** i + 1
+                i_heat = 3 ** i
                 j_heat = 2 ** j + 1
             heat[i_heat][j_heat] = 1
-            for n in range(i_heat-1,i_heat+2): # sources de chaleur
+            print(i,j,i_heat,j_heat)
+            for n in range(i_heat,i_heat+2): # sources de chaleur
                 for m in range(j_heat-1,j_heat+2):
                     if n == i_heat and m == j_heat:
                         pass
                     else:
-                        heat[n][m] = 2
+                        heat[n][m] = 1
             #DATA.append("Indice de gel au capteur " + str(j+1) + " de la rangée " + str(i+1))
 #display
 print('\n')
