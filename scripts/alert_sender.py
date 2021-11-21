2ced51c76bc5f5adaea117e7b2b582d966c47bca
Jonathan Oreste Maruca <marj3220@usherbrooke.ca>
2021-11-21 11:02:05 -0500

    Testing on RPi

--- scripts/alert_sender.py
+++ scripts/alert_sender.py
@@ -2,12 +2,12 @@ import access_tokens
 from twilio.rest import Client
 
 class AlertSender():
-    def __init__(self) -> None:
+    def __init__(self):
         account_sid = access_tokens.account_sid
         auth_token = access_tokens.auth_token
         self.client = Client(account_sid, auth_token)
     
-    def sendAlert(self, message: str):
+    def sendAlert(self, message):
         message = self.client.messages.create(
                             body =  message, #Message you send
                             from_ = access_tokens.api_phone_number,
