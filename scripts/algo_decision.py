e456103870358b1b68e46147c81757f27c3ab461
Miklab65 <93946073+Miklab65@users.noreply.github.com>
2021-11-22 15:29:23 -0500

    Update algo_decision.py

--- scripts/algo_decision.py
+++ scripts/algo_decision.py
@@ -2,26 +2,13 @@
 
 DATA: list = []
 
-ree=[
-    [1,0,0],
-    [0,0,1],
-    ]
-
-DATA.append(ree)
-DATA.append(ree)
-DATA.append(ree)
-DATA.append(ree)
-#alerter = AlertSender()
-
 last_T=[
-    [5,5,5],
     [5,5,5],
     [5,5,5]
     ]
 T=[
-    [5,5,5],
     [3,5,5],
-    [3,5,3]
+    [3,5,3],
     ]
 nb_strip = len(T) # nombre de strip de capteur
 nb_sensor = len(T[0]) # nombre de capteur par strip
@@ -36,7 +23,7 @@ for i in range(nb_strip):
                 #alerter.sendAlert("Indice de gel au capteur " + str(j+1) + " de la rangée " + str(i+1))
             if i == 0 and j == 0:
                 i_heat = i
-                j_heat = j + 1 
+                j_heat = j + 1
             elif i == 0:
                 i_heat = i
                 j_heat = 2 ** j + 1
@@ -50,21 +37,17 @@ for i in range(nb_strip):
             heat[i_heat][j_heat] = 1
             print(i,j,i_heat,j_heat)
             for n in range(i_heat,i_heat+2): # sources de chaleur
-                print(n)
                 for m in range(j_heat-1,j_heat+2):
-                    if n == i_heat and m == j_heat: # tjrs nécéssaire???
+                    if n == i_heat and m == j_heat:
                         pass
                     else:
                         heat[n][m] = 1
             if i != nb_strip - 1 and T[i+1][j] <= T_seuil: # chauffage du centre 
                 i_hc = i_heat + 2
                 for c in range(j_heat-1,j_heat+2):
-                    heat[i_hc][c] = 1
-                
+                    heat[i_hc][c] = 1 
             #DATA.append("Indice de gel au capteur " + str(j+1) + " de la rangée " + str(i+1))
-        
 #display
-print('\n')
 print('Nombre de bandes : ',nb_strip)
 print('Nombre de capteurs par bandes :',nb_sensor)
 print('map = ',len(heat),'x',len(heat[0]))
@@ -74,10 +57,10 @@ for i in range(len(last_T)):
 print('Nouvelles températures :')
 for i in range(nb_strip):
     print(T[i])
-print('états des capteurs (T) :')
+print('Seuil de température :',T_seuil,'Celsius')
+print('États des capteurs de température :')
 for i in range(nb_strip):
     print(T_state[i])
-print('Mapping du vignoble :')
+print('États des sources de chaleurs :')
 for i in range(len(heat)):
     print(heat[i])
-print('\n')    
